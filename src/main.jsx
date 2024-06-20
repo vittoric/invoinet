8238cc01b9fe24b00d9ffaada726784f01e784a8
vittoric <toricod22@gmail.com>
2024-06-21 00:17:10 +0200

    first

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './components/App.jsx'
+import NavBar from './components/Nav.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <NavBar />  
+    <App />
+    <img src="logo.png" alt="" srcset="" />
+
+  </React.StrictMode>,
+)
