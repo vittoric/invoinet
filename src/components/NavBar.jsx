b9182746db13292a0516291061dfa6504bdabb10
vittoric <toricod22@gmail.com>
2024-06-21 12:21:48 +0200

    navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -33,16 +33,12 @@ const NavBar = () => {
         </div>
       </div>
       {isOpen && (
-        <div className="lg:hidden bg-white shadow-md absolute top-20 left-0 w-full px-4 py-2">
+        <div className="lg:hidden bg-white shadow-md absolute top-20 left-0 w-full h-full px-4 py-4  text-center">
           <a href="#inicio" className="block text-gray-600 hover:text-black roboto-regular text-18px mb-2">Inicio</a>
           <a href="#nosotros" className="block text-gray-600 hover:text-gray-800 roboto-regular text-18px mb-2">Nosotros</a>
           <a href="#page" className="block text-gray-600 hover:text-gray-800 roboto-regular text-18px mb-2">Page</a>
-          <button className="block w-full bg-gradient-to-b from-customTealLight to-customTealDark text-white px-6 py-2 custom-height rounded-full text-16px mb-2">
-            Conversa con un experto
-          </button>
-          <button className="block w-full border border-gray-400 text-gray-600 px-6 py-2 custom-height rounded-full text-16px">
-            Iniciar sesión
-          </button>
+          <a href="#experto" className="block text-gray-600 hover:text-gray-800 roboto-regular text-18px mb-2">Conversa con un experto</a>
+          <a href="#sesion" className="block text-gray-600 hover:text-gray-800 roboto-regular text-18px mb-2">Iniciar sesión</a>
         </div>
       )}
     </nav>
