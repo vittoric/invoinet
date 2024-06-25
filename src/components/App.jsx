5cc17f3860f640003227ff6bb0f153278b153076
vittoric <toricod22@gmail.com>
2024-06-25 15:18:30 +0200

    logos

--- src/components/App.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-export default function App() {
-  return (
-    <div className="px-80 py-80">
-   <img src="https://media1.tenor.com/m/cFkRAYra5ggAAAAd/alone.gif" alt=""  />
-   </div>
-  )
-}
--- src/components/LogosComp.jsx
+++ src/components/LogosComp.jsx
@@ -2,29 +2,28 @@ import React from 'react';
 import nestleLogo from '../assets/nestle.png'; // Adjust the paths to your logo images
 import exxonLogo from '../assets/exonmobil.png';
 import revlonLogo from '../assets/revlon.png';
+import Marquee from "react-fast-marquee";
 
 const logos = [nestleLogo, exxonLogo, revlonLogo, nestleLogo, exxonLogo, revlonLogo]; // Add logos as needed
 
 const LogosComp = () => {
   return (
     <div className="bg-white py-8">
-      <div className="max-w-7xl mx-auto text-center mb-4">
-        <p className="text-gray-700 text-lg">
+      <div className="max-w-7xl mx-auto text-center mb-12">
+        <p className="text-black text-lg roboto-regular subheading">
           +40 empresas de bienes de consumo de Latinoamérica confían en InvoiNet:
         </p>
       </div>
-      <div className="overflow-hidden relative">
-        <div className="marquee whitespace-nowrap">
-          {logos.map((logo, index) => (
-            <img
-              key={index}
-              src={logo}
-              alt="Company logo"
-              className="inline-block mx-8 h-16"
-            />
-          ))}
-        </div>
-      </div>
+      <Marquee pauseOnHover gradient={false} speed={50}>
+        {logos.map((logo, index) => (
+          <img
+            key={index}
+            src={logo}
+            alt="Company logo"
+            className="inline-block mx-8 h-16"
+          />
+        ))}
+      </Marquee>
     </div>
   );
 };
